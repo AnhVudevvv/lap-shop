57a37e465e21907ee48cfc3a5759d241de787a8c
AnhVu <Tranvu051004@gmail.com>
2025-06-07 16:02:41 +0700

    Day4 : Home banner

--- src/components/navbar/brand/index.tsx
+++ src/components/navbar/brand/index.tsx
@@ -25,13 +25,13 @@ const dataBrands = [
 ];
 const Brand = () => {
     return (
-        <div className='px-12 '>
-            <h1 className="text-center text-2xl font-bold">Thương Hiệu Nổi Bật</h1>
-            <div className='grid  md:grid-cols-3 lg:grid-cols-6 gap-6'>
+        <div >
+            <h1 className="mt-5 text-center text-2xl font-bold">Thương Hiệu Nổi Bật</h1>
+            <div className='mt-5 grid grid-cols-2  md:grid-cols-5 lg:grid-cols-10 gap-6'>
                 {dataBrands.map((item: BrandType, index: number) => (
                     <div key={index}>
-                        <div className="rounded-xl p-6 text-center w-[200px] shadow-lg hover:shadow-lg/20 cursor-pointer hover:scale-105 transition-transform">
-                            <div className="bg-teal-800 text-white w-16 h-16 rounded-full mb-4 text-center flex justify-center items-center m-auto">
+                        <div className="rounded-xl p-3 text-center shadow-lg hover:shadow-lg/20 cursor-pointer hover:scale-105 transition-transform bg-[#e3faff]">
+                            <div className="bg-teal-800 text-white w-10 h-10 rounded-full mb-4 text-center flex justify-center items-center m-auto">
                                 <p className="font-bold text-2xl">{item.character}</p>
                             </div>
                             <p className="font-bold">{item.name}</p>
